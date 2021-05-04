d7f5e125c8a7ee380c25309890e3c5110da24595
plusw <a2833772178@163.com>
2021-05-04 21:55:09 +0800

    add

---
+++ demo18voc2012/main.py
@@ -0,0 +1,34 @@
+import d2lzh as d2l
+from mxnet import gluon, image, nd
+from mxnet.gluon import data as gdata, utils as gutils
+import os
+import sys
+import tarfile
+
+def download_voc_pascal(data_dir='../data'):
+    voc_dir = os.path.join(data_dir, 'VOCdevkit/VOC2012')
+    url = ('http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar')
+    sha1 = '4e443f8a2eca6b1dac8a6c57641b67dd40621a49'
+    fname = gutils.download(url, data_dir, sha1_hash=sha1)
+    with tarfile.open(fname, 'r') as f:
+        f.extractall(data_dir)
+    return voc_dir
+
+#voc_dir = download_voc_pascal()
+def read_voc_images(root=os.path.join('./data', 'VOCdevkit/VOC2012'), is_train=True):
+    txt_fname = '%s/ImageSets/Segmentation/%s' % (
+        root, 'train.txt' if is_train else 'val.txt')
+    with open(txt_fname, 'r') as f:
+        images = f.read().split()
+    features, labels = [None] * len(images), [None] * len(images)
+    for i, fname in enumerate(images):
+        features[i] = image.imread('%s/JPEGImages/%s.jpg' % (root, fname))
+        labels[i] = image.imread(
+            '%s/SegmentationClass/%s.png' % (root, fname))
+    return features, labels
+
+train_features, train_labels = read_voc_images()
+n=5
+imgs = train_features[0:n] + train_labels[0:n]
+d2l.show_images(imgs, 2, n);
+print(d2l.show_images.__code__)
